c6c6c7353f71df6c1c593e81a23d242342fc1719
Hemanth Kumar <hemanth.kumar@thoughtworks.com>
2022-06-27 18:24:28 +0530

    Initial Commit

---
+++ src/main/java/com/hemanth/todoAssignment/TodoAssignmentApplication.java
@@ -0,0 +1,11 @@
+package com.hemanth.todoAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAssignmentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAssignmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hemanth/todoAssignment/TodoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.todoAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
