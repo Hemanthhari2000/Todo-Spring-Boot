f6da6e1867b2423c90b3989f013884a7ba0f57a6
Hemanth Kumar <hemanth.kumar@thoughtworks.com>
2022-06-28 16:12:45 +0530

    Add delete todo functionality

--- src/test/java/com/hemanth/todoAssignment/todo/TodoRepositoryTest.java
+++ src/test/java/com/hemanth/todoAssignment/todo/TodoRepositoryTest.java
@@ -1,25 +1,21 @@
 package com.hemanth.todoAssignment.todo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
 public class TodoRepositoryTest {
 
     @Autowired
     private TodoRepository todoRepository;
-    Todo todo;
-    @BeforeEach
-    void setUp() {
-        todo = new Todo(1L, "Task #1", "This is the first todo task", false);
-    }
 
     @Test
     void shouldBeAbleToAddTodoTask() {
+        Todo todo = new Todo(1L, "Task #1", "This is the first todo task", false);
         todoRepository.save(todo);
 
         assertEquals(todoRepository.findById(todo.getId()).get(), todo);
@@ -27,12 +23,23 @@ public class TodoRepositoryTest {
 
     @Test
     void shouldBeAbleToUpdateAnExistingTodoTask() {
+        Todo todo = new Todo(2L, "Task #2", "This is the second todo task", false);
         todoRepository.save(todo);
-        String updatedDescription = "This is the [UPDATED] first todo task";
+        String updatedDescription = "This is the [UPDATED] second todo task";
 
         todo.setDescription(updatedDescription);
         todoRepository.save(todo);
 
-        assertEquals(todoRepository.findById(todo.getId()).get().getDescription(), updatedDescription);
+        assertEquals(todoRepository.findById(todo.getId()).get(), todo);
+    }
+
+    @Test
+    void shouldBeAbleToDeleteTheExistingTodoTask() {
+        Todo todo = new Todo(3L, "Task #3", "This is the three todo task", false);
+        todoRepository.save(todo);
+
+        todoRepository.deleteById(todo.getId());
+
+        assertFalse(todoRepository.existsById(todo.getId()));
     }
 }
