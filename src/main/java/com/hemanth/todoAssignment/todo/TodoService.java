a3cd86fc6f2129732313c7e10c10a1a2f42e5e94
Hemanth Kumar <hemanth.kumar@thoughtworks.com>
2022-06-30 13:46:58 +0530

    Add Todo Service to the application

---
+++ src/main/java/com/hemanth/todoAssignment/todo/TodoService.java
@@ -0,0 +1,38 @@
+package com.hemanth.todoAssignment.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public void add(Todo todo){
+        todoRepository.save(todo);
+    }
+
+    public Optional<Todo> getById(Long id) {
+        return todoRepository.findById(id);
+    }
+
+    public List<Todo> getAll() {
+        return todoRepository.findAll();
+    }
+
+    public void update(Todo todo) {
+        add(todo);
+    }
+
+    public void delete(Todo todo) {
+        todoRepository.delete(todo);
+    }
+}
---
+++ src/test/java/com/hemanth/todoAssignment/todo/TodoServiceTest.java
@@ -0,0 +1,62 @@
+package com.hemanth.todoAssignment.todo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+    private Todo todo;
+
+    @BeforeEach
+    void setUp() {
+        todo = new Todo(1L, "Task #1", "First Task", false);
+    }
+
+    @Test
+    void shouldBeAbleToAddTodoTaskUsingTodoService() {
+        todoService.add(todo);
+
+        verify(todoRepository).save(todo);
+    }
+
+    @Test
+    void shouldBeAbleToGetTodoByIdUsingTodoService() {
+        todoService.getById(todo.getId());
+
+        verify(todoRepository).findById(todo.getId());
+    }
+
+    @Test
+    void shouldBeAbleToGetAllTodoUsingTodoService() {
+        todoService.getAll();
+
+        verify(todoRepository).findAll();
+    }
+
+    @Test
+    void shouldBeAbleToUpdateTheExistingTodoUsingTodoService() {
+        todoService.update(todo);
+
+        verify(todoRepository).save(todo);
+    }
+
+    @Test
+    void shouldBeAbleToDeleteTheExistingTodoUsingTodoService() {
+        todoService.delete(todo);
+
+        verify(todoRepository).delete(todo);
+    }
+}
