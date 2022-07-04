b28b273c91a7ca41bd92388eac98209906111178
Hemanth Kumar <hemanth.kumar@thoughtworks.com>
2022-07-04 13:13:15 +0530

    Add Jasypt to encrypt database password

--- src/main/java/com/hemanth/todoAssignment/TodoAssignmentApplication.java
+++ src/main/java/com/hemanth/todoAssignment/TodoAssignmentApplication.java
@@ -1,9 +1,11 @@
 package com.hemanth.todoAssignment;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TodoAssignmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAssignmentApplication.class, args);
